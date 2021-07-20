d71d84b778fd552b1def22ea6f3d4b4a215cfeb8
Milind Mishra <milind.mishra4@gmail.com>
2021-07-20 11:45:59 +0530

    words++

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
 const items = [
-    "apple",
-    "apricot",
-    "banana",
-    "pear",
-    "guava",
-    "cherry",
-    "orange",
-    "pineapple",
-    "mango",
-    "grapes",
-    "blueberry",
-    "raspberry",
-    "melon",
-    "blackberry",
-    "plum",
-    "kiwi",
-    "peach",
-    "strawberry",
-    "avocado"
+  "apple",
+  "ape",
+  "apricot",
+  "banana",
+  "banyan",
+  "pear",
+  "peach",
+  "guava",
+  "goal",
+  "cherry",
+  "orange",
+  "orangotan",
+  "pineapple",
+  "pink",
+  "mango",
+  "man",
+  "grapes",
+  "gum",
+  "blueberry",
+  "raspberry",
+  "melon",
+  "muskmelon",
+  "blackberry",
+  "plum",
+  "pluck",
+  "kiwi",
+  "knife",
+  "peach",
+  "strawberry",
+  "avocado",
+  "armadillo",
 ];
 
-const root = new makeNode('\0');
-for (const item of items)
-    add(item, 0, root);
+const root = new makeNode("\0");
+for (const item of items) add(item, 0, root);
 
 const text_box = document.getElementById("text-box");
 const list = document.getElementById("list");
 
 function handler(e) {
-    const str = e.target.value;
-    const predictions = search(str, 0, root);
+  const str = e.target.value;
+  const predictions = search(str, 0, root);
 
-    console.log(predictions);
+  console.log(predictions);
 
-    list.innerHTML = "";
-    for (const prediction of predictions)
-        list.innerHTML += `<li class="list-group-item clickable" onclick="handleClick(this)"><b>${str}</b>${prediction.substring(str.length)}</li>`;
-
+  list.innerHTML = "";
+  for (const prediction of predictions)
+    list.innerHTML += `<li class="list-group-item clickable" onclick="handleClick(this)"><b>${str}</b>${prediction.substring(
+      str.length
+    )}</li>`;
 }
 
 function handleClick(e) {
-    text_box.value = e.innerText;
+  text_box.value = e.innerText;
 }
 
 handler({ target: { value: "" } });
-
 
 text_box.addEventListener("keyup", handler);
